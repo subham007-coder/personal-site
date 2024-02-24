713a68f63037fa15361317222b3b012c233359e9
subham007-coder <dsubham109@gmail.com>
2024-02-24 20:51:51 +0530

    add

--- script.js
+++ script.js
@@ -6,8 +6,29 @@ const scroll = new LocomotiveScroll({
 
 gsap.from('.ani', {
     x: -1700,
-    duretion: 0.5,
+    duretion: 0.2,
     delay: 0.5,
     stagger: 1
 });
 
+gsap.from('.ani2', {
+    x: -1700,
+    duretion: 2,
+    delay: 2,
+    stagger: 1
+});
+
+
+// hambergar
+// let option = document.querySelector(".hambarger");
+// let nav1 = document.querySelector(".nav-part1");
+
+// option.addEventListener("click", () => {
+//     if (option.classList == "hambarger") {
+//         option.classList.add("add");
+//         nav1.style.visibility = "visible"
+//     } else{
+//         option.classList = "hambarger";
+//         nav1.style.visibility = "hidden"
+//     }
+// });
